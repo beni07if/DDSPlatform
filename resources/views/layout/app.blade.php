d798ce49dad44215145c5c069312381e6f2a28c0
beni07if <beni07elreal@gmail.com>
2023-11-18 20:57:22 +0700

    Update landing page1

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -42,9 +42,9 @@
   <header id="header" class="fixed-top d-flex align-items-center header-transparent">
     <div class="container d-flex align-items-center">
 
-      <h1 class="logo me-auto"><a href="#"><b>Inovasi Agriplot</b></a></h1>
+      <h1 class="logo me-auto"><a href="#"><img src="{{ asset('asset/Rapid/assets/img/logo-agriplot2.png') }}" alt="" class="img-fluid"><b>Inovasi Agriplot</b></a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
-      <!-- <a href="#" class="logo me-auto"><img src="{{ asset('asset/Rapid/assets/img/logo.png') }}" alt="" class="img-fluid"></a>-->
+      <!-- <a href="#" class="logo me-auto"><img src="{{ asset('asset/Rapid/assets/img/logo-agriplot.png') }}" alt="" class="img-fluid"></a> -->
 
       <nav id="navbar" class="navbar order-last order-lg-0">
         <ul>
